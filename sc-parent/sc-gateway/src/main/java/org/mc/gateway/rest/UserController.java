8ecd8d6a8e6c28770502de0a956b503ae7f85d48
xumaocai <xumaocai@wherecom.com>
2016-08-18 20:03:39 +0800

    测试中

---
+++ sc-parent/sc-gateway/src/main/java/org/mc/gateway/rest/UserController.java
@@ -0,0 +1,18 @@
+package org.mc.gateway.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+	@Value("${user.name}")
+	private String val;
+	
+	@RequestMapping(value = "/getVal",method = RequestMethod.GET)
+	public String getVal(){
+		return val;
+	}
+}
