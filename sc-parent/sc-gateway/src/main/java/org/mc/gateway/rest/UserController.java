7c136566cac15bc3f3d9ec1f84b240b986ee5184
xumaocai <xumaocai@wherecom.com>
2016-08-18 20:41:51 +0800

    cs

--- sc-parent/sc-gateway/src/main/java/org/mc/gateway/rest/UserController.java
+++ sc-parent/sc-gateway/src/main/java/org/mc/gateway/rest/UserController.java
@@ -1,6 +1,8 @@
 package org.mc.gateway.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 	@Value("${user.name}")
 	private String val;
+	@Autowired
+	private Environment  env;
 	
 	@RequestMapping(value = "/getVal",method = RequestMethod.GET)
 	public String getVal(){
 		System.out.println(val);
+		System.out.println(env.getProperty("user.name"));
 		return val;
 	}
 }
